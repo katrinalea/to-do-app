e5b7849b9a0fb156402056886c44f61def62f711
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-30 15:23:02 +0000

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { greet } from "./utils/greet";
+import ToDo from "./to-do"
+import "./toDo.css"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+ <ToDo />
+  )
 }
 
 export default App;
---
+++ src/to-do.tsx
@@ -0,0 +1,14 @@
+
+
+export default function ToDo (): JSX.Element {
+    return (
+        <>
+        <div className = "page">
+        <h1> To Do List</h1>
+        <input type = "text"  placeholder = "Input a ToDo item"/>
+        <p> This is what you need to do:</p>
+        </div>
+        </>
+    )
+
+}

4ccb2cbb1ba3b719079c91c5dcb7e6bb648f5a74
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-30 16:39:49 +0000

    list Ids

--- src/to-do.tsx
+++ src/to-do.tsx
@@ -1,12 +1,46 @@
 
+import axios from "axios";
+import {useEffect, useState} from "react"
 
+interface ToDoInterface {
+    id: number;
+    message: string
+}
 export default function ToDo (): JSX.Element {
+    const [allTasks, setAllTasks] = useState<ToDoInterface[]>([])
+
+ 
+    
+    useEffect (() => {
+        const fetchAPI = async () => {
+            const response = await axios.get("https://todo-backend-bfou.onrender.com/items");
+            setAllTasks(response.data)
+        };  
+        console.log(allTasks)
+        fetchAPI()
+    }, [])
+
+    console.log({allTasks})
+
+   
+    
+
+
     return (
         <>
         <div className = "page">
         <h1> To Do List</h1>
         <input type = "text"  placeholder = "Input a ToDo item"/>
         <p> This is what you need to do:</p>
+        <p> 
+            <ul>
+               {allTasks.map((item: ToDoInterface, index: number) => (
+       <li key ={index} >{item.id}</li>))}
+    </ul>
+
+        
+        
+    </p>
         </div>
         </>
     )

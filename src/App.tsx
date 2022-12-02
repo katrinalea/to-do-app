682fca8a1472d53ff8727fa87090b446fb12234b
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-12-02 14:49:25 +0000

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import ToDo from "./to-do"
-import "./toDo.css"
+import ToDo from "./to-do";
+import "./toDo.css";
 
 function App(): JSX.Element {
-  return (
- <ToDo />
-  )
+  return <ToDo />;
 }
 
 export default App;
--- src/to-do.tsx
+++ src/to-do.tsx
@@ -1,88 +1,109 @@
-
 import axios from "axios";
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 
 interface ToDoInterface {
-    id: number;
-    message: string
+  id: number;
+  message: string;
 }
-export default function ToDo (): JSX.Element {
-    const [allTasks, setAllTasks] = useState<ToDoInterface[]>([])
-    const [toDoItem, setToDoItem] = useState<string>("")
-    const [completedTasks, setCompletedTasks] = useState<ToDoInterface[]>([])
+export default function ToDo(): JSX.Element {
+  const [allTasks, setAllTasks] = useState<ToDoInterface[]>([]);
+  const [toDoItem, setToDoItem] = useState<string>("");
+  const [completedTasks, setCompletedTasks] = useState<ToDoInterface[]>([]);
 
- 
-    
-    useEffect (() => {
-        const fetchAPI = async () => {
-            //using axios to fetch the API, using a get command, assigning the vraible 'response' to theAPI info
-            const response = await axios.get("https://todo-backend-bfou.onrender.com/items");
-            // assigning fetchedTasks to the API data, specifically data
-            const fetchedTasks = response.data;
-            // sets tasks to the data
-            setAllTasks(fetchedTasks)
-        };  
-        fetchAPI()
-    }, [allTasks])
+  useEffect(() => {
+    const fetchAPI = async () => {
+      //using axios to fetch the API, using a get command, assigning the vraible 'response' to theAPI info
+      const response = await axios.get(
+        "https://todo-backend-bfou.onrender.com/items"
+      );
+      // assigning fetchedTasks to the API data, specifically data
+      const fetchedTasks = response.data;
+      // sets tasks to the data
+      setAllTasks(fetchedTasks);
+    };
+    fetchAPI();
+  }, [allTasks]);
 
-    useEffect (() => {
-        const fetchAPI = async () => {
-            //using axios to fetch the API, using a get command, assigning the vraible 'response' to theAPI info
-            const response = await axios.get("https://todo-backend-bfou.onrender.com/completed");
-            // assigning fetchedTasks to the API data, specifically data
-            const fetchedCompletedTasks = response.data;
-            // sets tasks to the data
-            setCompletedTasks(fetchedCompletedTasks)
-            console.log("fetched completed")
-        };  
-        fetchAPI()
-    }, [completedTasks])
+  // fetches from the completed api everytime completed tasks is updated
+  useEffect(() => {
+    const fetchAPI = async () => {
+      //using axios to fetch the API, using a get command, assigning the vraible 'response' to theAPI info
+      const response = await axios.get(
+        "https://todo-backend-bfou.onrender.com/completed"
+      );
+      // assigning fetchedTasks to the API data, specifically data
+      const fetchedCompletedTasks = response.data;
+      // sets tasks to the data
+      setCompletedTasks(fetchedCompletedTasks);
+      console.log("fetched completed");
+    };
+    fetchAPI();
+  }, [completedTasks]);
 
+  //adds an item to the all items api
+  const handleAddItem = async (item: string) => {
+    const response = await axios.post(
+      "https://todo-backend-bfou.onrender.com/items",
+      { message: item }
+    );
+    console.log(response);
+  };
 
-    const handleAddItem = async (item: string) => {
-        const response = await axios.post("https://todo-backend-bfou.onrender.com/items", {message: item});
-        console.log(response)
-    }
+  // deletes the item from the all items api
+  const handleDeleteItem = async (number: number) => {
+    const response = await axios.delete(
+      `https://todo-backend-bfou.onrender.com/items/${number}`,
+      { data: { id: number } }
+    );
+    console.log(response);
+  };
 
-    const handleDeleteItem = async (number: number) => {
-        const response = await axios.delete(`https://todo-backend-bfou.onrender.com/items/${number}`, {data: {id: number}});
-        console.log(response)
-        console.log([...allTasks], `instructed to delete ${number}` )
-    }
-    
-    const handleCompletedItem = async (number: number, message: string) => {
-        await axios.delete(`https://todo-backend-bfou.onrender.com/items/${number}`, {data: {id: number}});
-        const response = await axios.post("https://todo-backend-bfou.onrender.com/completed", {message});
-        console.log(response);
-        console.log(`${number}`)
-    }
+  // deleting the task from the all items api, then posts to the completed api
+  const handleCompletedItem = async (number: number, message: string) => {
+    await axios.delete(
+      `https://todo-backend-bfou.onrender.com/items/${number}`,
+      { data: { id: number } }
+    );
+    const response = await axios.post(
+      "https://todo-backend-bfou.onrender.com/completed",
+      { message }
+    );
+    console.log(response);
+  };
 
-    
-        
-     
-    
-// our toDo input  wants to assign a message
-
-    return (
-        <>
-        <div className = "page">
+  return (
+    <>
+      <div className="page">
         <h1> To Do List</h1>
-        <input type = "text"  placeholder = "Input a ToDo item" onChange = {(e) => setToDoItem(e.target.value)}/>
-        <button onClick = {() => handleAddItem(toDoItem)}> Submit</button>
-        <p> This is what you need to do:</p>
-            <ul>
-               {allTasks.map((item: ToDoInterface) => (
-       <li key ={item.id} >{item.message} 
-       <button onClick = {() => handleDeleteItem(item.id)} >🗑️</button>
-       <button onClick = {() => handleCompletedItem(item.id, item.message)} >✅</button>
-        </li>))} 
-    </ul>
-    <p> Your completed tasks:</p>
-    <ul>{completedTasks.map((item: ToDoInterface, index)=> (
-        <li key = {item.id}>{item.message}</li>))}
-    </ul>
-        </div>
-        </>
-    )
-
+        <input
+          type="text"
+          placeholder="Input a ToDo item"
+          onChange={(e) => setToDoItem(e.target.value)}
+        />
+        <button onClick={() => handleAddItem(toDoItem)}> Submit</button>
+        <hr />
+        <h3> This is what you need to do:</h3>
+        <ul className="list">
+          {allTasks.map((item: ToDoInterface) => (
+            <li key={item.id}>
+              {item.message}
+              <button onClick={() => handleDeleteItem(item.id)}>🗑️</button>
+              <button
+                onClick={() => handleCompletedItem(item.id, item.message)}
+              >
+                ✅
+              </button>
+            </li>
+          ))}
+        </ul>
+        <hr />
+        <h3> Your completed tasks:</h3>
+        <ul className="list">
+          {completedTasks.map((item: ToDoInterface, index) => (
+            <li key={item.id}>{item.message}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }

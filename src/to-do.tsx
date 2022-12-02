93940024c391312fc10db17b1bc3da62a4ae393a
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-12-02 14:40:50 +0000

    added completed option

--- src/to-do.tsx
+++ src/to-do.tsx
@@ -9,6 +9,7 @@ interface ToDoInterface {
 export default function ToDo (): JSX.Element {
     const [allTasks, setAllTasks] = useState<ToDoInterface[]>([])
     const [toDoItem, setToDoItem] = useState<string>("")
+    const [completedTasks, setCompletedTasks] = useState<ToDoInterface[]>([])
 
  
     
@@ -24,17 +25,37 @@ export default function ToDo (): JSX.Element {
         fetchAPI()
     }, [allTasks])
 
+    useEffect (() => {
+        const fetchAPI = async () => {
+            //using axios to fetch the API, using a get command, assigning the vraible 'response' to theAPI info
+            const response = await axios.get("https://todo-backend-bfou.onrender.com/completed");
+            // assigning fetchedTasks to the API data, specifically data
+            const fetchedCompletedTasks = response.data;
+            // sets tasks to the data
+            setCompletedTasks(fetchedCompletedTasks)
+            console.log("fetched completed")
+        };  
+        fetchAPI()
+    }, [completedTasks])
+
+
     const handleAddItem = async (item: string) => {
         const response = await axios.post("https://todo-backend-bfou.onrender.com/items", {message: item});
         console.log(response)
     }
 
     const handleDeleteItem = async (number: number) => {
-        const response = await axios.delete(`https://todo-backend-bfou.onrender.com/items/`, {data: {id: number}});
+        const response = await axios.delete(`https://todo-backend-bfou.onrender.com/items/${number}`, {data: {id: number}});
         console.log(response)
-        console.log([...allTasks])
+        console.log([...allTasks], `instructed to delete ${number}` )
     }
-    // need to delete specific item from API
+    
+    const handleCompletedItem = async (number: number, message: string) => {
+        await axios.delete(`https://todo-backend-bfou.onrender.com/items/${number}`, {data: {id: number}});
+        const response = await axios.post("https://todo-backend-bfou.onrender.com/completed", {message});
+        console.log(response);
+        console.log(`${number}`)
+    }
 
     
         
@@ -51,7 +72,14 @@ export default function ToDo (): JSX.Element {
         <p> This is what you need to do:</p>
             <ul>
                {allTasks.map((item: ToDoInterface) => (
-       <li key ={item.id} >{item.message} <button onClick = {() => handleDeleteItem(item.id)} > Delete item</button> </li>))} 
+       <li key ={item.id} >{item.message} 
+       <button onClick = {() => handleDeleteItem(item.id)} >🗑️</button>
+       <button onClick = {() => handleCompletedItem(item.id, item.message)} >✅</button>
+        </li>))} 
+    </ul>
+    <p> Your completed tasks:</p>
+    <ul>{completedTasks.map((item: ToDoInterface, index)=> (
+        <li key = {item.id}>{item.message}</li>))}
     </ul>
         </div>
         </>
